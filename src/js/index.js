64c8839bc5d3c88fdfdf774e6686cd2bb7380f86
Abdallah <abdallah.moubarak02@gmail.com>
2022-09-10 14:59:22 +0300

    fetching dog images

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,9 @@
-window.addEventListener("DOMContentLoaded", () => {});
+window.addEventListener("DOMContentLoaded", () => {
+  //   initializing varables
+  const btn = document.getElementById("predict-button");
+
+  //   fetching dog images
+  fetch("https://dog.ceo/api/breeds/image/random")
+    .then((response) => response.json())
+    .then((data) => (document.getElementById("dogImg").src = data.message));
+});
